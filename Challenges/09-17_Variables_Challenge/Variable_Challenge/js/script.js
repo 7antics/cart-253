5e6d812f30de5ca8b43c9a24a0ed7399fe537214
Anna Tsirbas <annatsirbas@gmail.com>
2025-09-17 21:05:28 -0400

    Updated Mr.Furious and his pet peeves

--- Challenges/09-17_Variables_Challenge/Variable_Challenge/js/script.js
+++ Challenges/09-17_Variables_Challenge/Variable_Challenge/js/script.js
@@ -7,6 +7,43 @@
 
 "use strict";
 
+let bird = {
+pos: {
+x: 100,
+y: 100,
+},
+fill: {
+r: 70,
+g: 70,
+b: 255,
+},
+stroke: {
+r: 255,
+g: 255,
+b: 255,
+},
+bodySize: {
+x: 40,
+y: 20,
+},
+wingSize: {
+x: 15,
+y: 40,
+},
+beakSize: {
+x: 10,
+y: 3,
+},
+beakFill: {
+r: 252,
+g: 115,
+b: 3,
+},
+velocity: 1
+}
+
+let movingToRight = true;
+
 // Our friend Mr. Furious
 let mrFurious = {
   // Position and size
@@ -21,18 +58,6 @@ let mrFurious = {
   },
 };
 
-let bird = {
-  //Position and Size
-  birdX: 50,
-  birdY: 50,
-  birdSize: 25,
-  //Colour
-  fill: {
-    birdRed: 255,
-    birdGreen: 174,
-    birdBlue: 66,
-  },
-};
 let skyBackground = {
   skyRed: 173,
   skyGreen: 216,
@@ -56,7 +81,27 @@ function draw() {
     skyBackground.skyBlue
   );
 
-  //Bird
+  
+drawBird();
+
+if(movingToRight)
+{
+birdMoveR();
+if(bird.pos.x >= 300)
+{
+movingToRight = false;
+}
+}
+
+else if(!movingToRight)
+{
+birdMoveL();
+
+if(bird.pos.x <= 100)
+{
+movingToRight = true;
+}
+}
 
   // Make Mr.Furious red from rage!
   push();
@@ -66,14 +111,30 @@ function draw() {
 
   //Make Mr.Furious shake from rage!
   mrFurious.x = mrFurious.x + 1;
-  mrFurious.x = constrain(2, 199, 202);
 
+  if (mrFurious.x == 210) {
+    mrFurious.x = mrFurious.x - 5;
+  }
+  else if (mrFurious.x == 200) {
+    mrFurious.x = mrFurious.x + 5;
+  }
+
+  
 
   // Draw Mr. Furious as a coloured circle
   push();
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+  push();
+  stroke(5);
+  // Mr.Furious' Glare
+  push();
+  arc(mrFurious.x - 5, mrFurious.y - 5, 15, 15, 0, PI, OPEN);
+  arc(mrFurious.x - 10, mrFurious.y - 10, 15, 15, 0, PI, OPEN);
+  // Mr.Furious' Frown
+  arc(mrFurious.x - 10, mrFurious.y - 10, 15, 15, PI, 0, OPEN);
   pop();
 
   //Sky goes from Day to Night
@@ -83,3 +144,44 @@ function draw() {
   skyBackground.skyBlue = skyBackground.skyBlue - 1;
   pop();
 }
+
+// Draw the bird
+function drawBird(){
+//draw the bird's body
+push();
+fill(bird.fill.r, bird.fill.g, bird.fill.b);
+noStroke();
+ellipse(bird.pos.x, bird.pos.y,bird.bodySize.x, bird.bodySize.y);
+//draw the bird's wings
+push();
+fill(bird.fill.r, bird.fill.g, bird.fill.b);
+noStroke();
+ellipse(bird.pos.x, bird.pos.y, bird.wingSize.x, bird.wingSize.y);
+//draw the bird's beak
+push();
+noStroke();
+fill(bird.beakFill.r, bird.beakFill.g, bird.beakFill.b);
+ellipse(bird.pos.x+20, bird.pos.y, bird.beakSize.x, bird.beakSize.y);
+pop();
+pop();
+pop();
+//draw the bird's eyes
+ellipse(bird.pos.x+10,bird.pos.y+3,3);
+ellipse(bird.pos.x + 10, bird.pos.y - 3, 3);
+  
+
+}
+
+//move the bird right
+function birdMoveR (){
+bird.velocity += .15;
+bird.velocity = constrain(bird.velocity, -2, 2);
+bird.pos.x += bird.velocity;
+}
+//move the bird left
+function birdMoveL (){
+bird.velocity -= .15;
+bird.velocity = constrain(bird.velocity, -2, 2);
+bird.pos.x += bird.velocity;
+}
+
