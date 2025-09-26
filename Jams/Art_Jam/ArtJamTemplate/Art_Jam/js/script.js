acf49bbd2f606becfe587d8414b57aaeabcbab67
Anna Tsirbas <annatsirbas@gmail.com>
2025-09-26 16:03:59 -0400

    fixed the clicking for each cards specific coordinates

--- Jams/Art_Jam/ArtJamTemplate/Art_Jam/js/script.js
+++ Jams/Art_Jam/ArtJamTemplate/Art_Jam/js/script.js
@@ -11,11 +11,10 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
  */
 
-// Cards
-let cardBack = {
+// Card Properties
+let cardBackLeft = {
   //Position and Size
   cardX: 50,
   cardY: 50,
@@ -25,18 +24,77 @@ let cardBack = {
   fill: {
     cardBackFill: "#4100F3",
     cardFaceFill: "#FFFFFF",
-
   },
 };
 
-// Placeholder of card pick result
-let heart = {
-    // Ellipse Size
-    ellipseSize: {
-        w: 10,
-        h: 10,
-    },
-    //Ellipse 1 Position
+let cardBackMiddle = {
+  //Position and Size
+  cardX: 250,
+  cardY: 50,
+  cardW: 150,
+  cardH: 300,
+  //Card Back Colour
+  fill: {
+    cardBackFill: "#4100F3",
+    cardFaceFill: "#FFFFFF",
+  },
+};
+
+let cardBackRight = {
+  //Position and Size
+  cardX: 450,
+  cardY: 50,
+  cardW: 150,
+  cardH: 300,
+  //Card Back Colour
+  fill: {
+    cardBackFill: "#4100F3",
+    cardFaceFill: "#FFFFFF",
+  },
+};
+
+// Heart Properties
+let heartLeft = {
+  // Ellipse Size
+  ellipseSize: {
+    w: 10,
+    h: 10,
+  },
+  //Ellipse 1 Position
+  ellipseOne: {
+    x: 275,
+    y: 400,
+  },
+  // Ellipse 2 Position
+  ellipseTwo: {
+    x: 285,
+    y: 400,
+  },
+  // Triangle Position
+  triPos: {
+    xOne: 270,
+    yOne: 402,
+    xTwo: 290,
+    yTwo: 402,
+    xThree: 280,
+    yThree: 420,
+  },
+  //Heart Colour
+  fill: "#FFFFFF",
+  fills: {
+    base: "#FFFFFF",
+    good: "#FF0000",
+    bad: "#808080"
+  }
+};
+
+let heartMiddle = {
+  // Ellipse Size
+  ellipseSize: {
+    w: 10,
+    h: 10,
+  },
+  //Ellipse 1 Position
   ellipseOne: {
     x: 325,
     y: 400,
@@ -56,11 +114,46 @@ let heart = {
     yThree: 420,
   },
   //Heart Colour
-  fill: {
-    r: 255,
-    g: 255,
-    b: 255,
+  fill: "#FFFFFF",
+  fills: {
+    base: "#FFFFFF",
+    good: "#FF0000",
+    bad: "#808080"
+  }
+};
+
+let heartRight = {
+  // Ellipse Size
+  ellipseSize: {
+    w: 10,
+    h: 10,
   },
+  //Ellipse 1 Position
+  ellipseOne: {
+    x: 375,
+    y: 400,
+  },
+  // Ellipse 2 Position
+  ellipseTwo: {
+    x: 385,
+    y: 400,
+  },
+  // Triangle Position
+  triPos: {
+    xOne: 370,
+    yOne: 402,
+    xTwo: 390,
+    yTwo: 402,
+    xThree: 380,
+    yThree: 420,
+  },
+  //Heart Colour
+  fill: "#FFFFFF",
+  fills: {
+    base: "#FFFFFF",
+    good: "#FF0000",
+    bad: "#808080"
+  }
 };
 
 function setup() {
@@ -69,128 +162,189 @@ function setup() {
   background(192, 234, 240);
 }
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
- */
 function draw() {
-
-  displayCardFace();
+ 
+  writeText();
   drawCardBack();
   drawHearts();
+  display();
+}
 
+function writeText() {
+  push();
+  //Colour of Text
+  fill("#FFFFFF");
+  //Size of text
+  textSize(20);
+  //Alignment of text
+ textAlign(CENTER, CENTER);
+  //Style of text
+  textStyle(BOLDITALIC);
+  //What the text says
+  text("Pick a card to make Anna's Perfect Day!", width / 2, 25);
+  pop();
 }
 
 function drawCardBack() {
-
   // Left Card Back
   push();
   noStroke();
-  fill(cardBack.fill.cardBackFill);
-  rect(cardBack.cardX, cardBack.cardY, cardBack.cardW, cardBack.cardH);
+  fill(cardBackLeft.fill.cardBackFill);
+  rect(
+    cardBackLeft.cardX,
+    cardBackLeft.cardY,
+    cardBackLeft.cardW,
+    cardBackLeft.cardH
+  );
   pop();
 
   // Middle Card Back
   push();
   noStroke();
-  fill(cardBack.fill.cardBackFill);
-  rect(cardBack.cardX + 200, cardBack.cardY, cardBack.cardW, cardBack.cardH);
+  fill(cardBackMiddle.fill.cardBackFill);
+  rect(
+    cardBackMiddle.cardX,
+    cardBackMiddle.cardY,
+    cardBackMiddle.cardW,
+    cardBackMiddle.cardH
+  );
   pop();
 
   // Right Card Back
   push();
   noStroke();
-  fill(cardBack.fill.cardBackFill);
-  rect(cardBack.cardX + 400, cardBack.cardY, cardBack.cardW, cardBack.cardH);
+  fill(cardBackRight.fill.cardBackFill);
+  rect(
+    cardBackRight.cardX,
+    cardBackRight.cardY,
+    cardBackRight.cardW,
+    cardBackRight.cardH
+  );
   pop();
-
 }
 
 function drawHearts() {
-    
-    // Draw Hearts
-    // Left Heart
+  // Draw Hearts
+  // Left Heart
   push();
   noStroke();
-  fill(heart.fill.r, heart.fill.g, heart.fill.b);
-    ellipse(
-        heart.ellipseOne.x - 50,
-        heart.ellipseOne.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    ellipse(
-        heart.ellipseTwo.x - 50,
-        heart.ellipseTwo.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    triangle(
-        heart.triPos.xOne - 50,
-        heart.triPos.yOne,
-        heart.triPos.xTwo - 50,
-        heart.triPos.yTwo,
-        heart.triPos.xThree - 50,
-        heart.triPos.yThree);
-    pop();
-
-    //Middle Heart
-  push();
-  noStroke();
-  fill(heart.fill.r, heart.fill.g, heart.fill.b);
-    ellipse(
-        heart.ellipseOne.x,
-        heart.ellipseOne.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    ellipse(heart.ellipseTwo.x,
-        heart.ellipseTwo.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    triangle(
-        heart.triPos.xOne,
-        heart.triPos.yOne,
-        heart.triPos.xTwo,
-        heart.triPos.yTwo,
-        heart.triPos.xThree,
-        heart.triPos.yThree);
-    
-    //Right Heart
-      push();
-  noStroke();
-  fill(heart.fill.r, heart.fill.g, heart.fill.b);
-    ellipse(
-        heart.ellipseOne.x + 50,
-        heart.ellipseOne.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    ellipse(
-        heart.ellipseTwo.x + 50,
-        heart.ellipseTwo.y,
-        heart.ellipseSize.w,
-        heart.ellipseSize.h);
-    triangle(
-        heart.triPos.xOne + 50,
-        heart.triPos.yOne,
-        heart.triPos.xTwo + 50,
-        heart.triPos.yTwo,
-        heart.triPos.xThree + 50,
-        heart.triPos.yThree);
+  fill(heartLeft.fill);
+  ellipse(
+    heartLeft.ellipseOne.x,
+    heartLeft.ellipseOne.y,
+    heartLeft.ellipseSize.w,
+    heartLeft.ellipseSize.h
+  );
+  ellipse(
+    heartLeft.ellipseTwo.x,
+    heartLeft.ellipseTwo.y,
+    heartLeft.ellipseSize.w,
+    heartLeft.ellipseSize.h
+  );
+  triangle(
+    heartLeft.triPos.xOne,
+    heartLeft.triPos.yOne,
+    heartLeft.triPos.xTwo,
+    heartLeft.triPos.yTwo,
+    heartLeft.triPos.xThree,
+    heartLeft.triPos.yThree
+  );
   pop();
 
+  //Middle Heart
+  push();
+  noStroke();
+  fill(heartMiddle.fill);
+  ellipse(
+    heartMiddle.ellipseOne.x,
+    heartMiddle.ellipseOne.y,
+    heartMiddle.ellipseSize.w,
+    heartMiddle.ellipseSize.h
+  );
+  ellipse(
+    heartMiddle.ellipseTwo.x,
+    heartMiddle.ellipseTwo.y,
+    heartMiddle.ellipseSize.w,
+    heartMiddle.ellipseSize.h
+  );
+  triangle(
+    heartMiddle.triPos.xOne,
+    heartMiddle.triPos.yOne,
+    heartMiddle.triPos.xTwo,
+    heartMiddle.triPos.yTwo,
+    heartMiddle.triPos.xThree,
+    heartMiddle.triPos.yThree
+  );
+
+  //Right Heart
+  push();
+  noStroke();
+  fill(heartRight.fill);
+  ellipse(
+    heartRight.ellipseOne.x,
+    heartRight.ellipseOne.y,
+    heartRight.ellipseSize.w,
+    heartRight.ellipseSize.h
+  );
+  ellipse(
+    heartRight.ellipseTwo.x,
+    heartRight.ellipseTwo.y,
+    heartRight.ellipseSize.w,
+    heartRight.ellipseSize.h
+  );
+  triangle(
+    heartRight.triPos.xOne,
+    heartRight.triPos.yOne,
+    heartRight.triPos.xTwo,
+    heartRight.triPos.yTwo,
+    heartRight.triPos.xThree,
+    heartRight.triPos.yThree
+  );
+  pop();
 }
 
-function displayCardFace() {
+function display() {
 
-  const distance = dist(mouseX, mouseY, cardBack.x, cardBack.y);
-  const mouseOverlapsCard = (distance < cardBack.size / 2);
-
-//If mouse clicks card back, card back changes colour to card face
-  if (mouseIsPressed) {
-    cardBack.fill.cardBackFill = cardBack.fill.cardFaceFill;
+//Left card constraints for when mouse is in card coordinates
+  let overlapsCardLeft = false
+  
+  if (mouseX > cardBackLeft.cardX && mouseX < cardBackLeft.cardX + cardBackLeft.cardW) {
+    if (mouseY > cardBackLeft.cardY && mouseY < cardBackLeft.cardY + cardBackLeft.cardH)
+    {overlapsCardLeft = true}
   }
 
+//Middle Card constraints for when mouse is in card coordinates
+  let overlapsCardMiddle = false
+
+  if (mouseX > cardBackMiddle.cardX && mouseX < cardBackMiddle.cardX + cardBackMiddle.cardW) {
+    if (mouseY > cardBackMiddle.cardY && mouseY < cardBackMiddle.cardY + cardBackMiddle.cardH)
+    { overlapsCardMiddle = true }
+  }
+
+  //Right Card constraints for when mouse is in card coordinates
+  let overlapsCardRight = false
+
+  if (mouseX > cardBackRight.cardX && mouseX < cardBackRight.cardX + cardBackRight.cardW) {
+    if (mouseY > cardBackRight.cardY && mouseY < cardBackRight.cardY + cardBackRight.cardH) 
+    { overlapsCardRight = true}
+  }
+
+  //If mouse clicks Left card back, card back changes colour to card face colour
+  if (overlapsCardLeft && mouseIsPressed) {
+    cardBackLeft.fill.cardBackFill = cardBackLeft.fill.cardFaceFill;
+  }
+
+  //If mouse click Middle card back, card back changes colour to card face colour
+  if (overlapsCardMiddle && mouseIsPressed) {
+    cardBackMiddle.fill.cardBackFill = cardBackMiddle.fill.cardFaceFill;
+  }
+
+  //If mouse clicks Right card back, card back changes colour to card face colour
+  if (overlapsCardRight && mouseIsPressed) {
+    cardBackRight.fill.cardBackFill = cardBackRight.fill.cardFaceFill;
+  }
   //When cardback changes to card face, introduce card result
-
 }
-
 
 //when card result appears, change colour of heart
 
@@ -203,3 +357,5 @@ function displayCardFace() {
 //If two red hearts display "End Scene Two/MEH."
 
 //If one red heart display "End Scene Three/WOMPWOMP!"
+
+//If no red hearts display "End Scene Four/WAH!"
