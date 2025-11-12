42b226ca6c2b5b9eae1f16a743c7048871b75a76
Anna Tsirbas <annatsirbas@gmail.com>
2025-11-12 13:54:39 -0500

    downloaded the car and greek titan name data folders, and named the project

--- Challenges/11-12_Data_Challenge/datachallenge/template/js/script.js
+++ Challenges/11-12_Data_Challenge/datachallenge/template/js/script.js
@@ -1,24 +1,62 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Naming My Car!
+ * Anna Tsirbas & Pippin Barr
+ *
+ * A program to generate new car model names using Titans from Greek Mythology.
+ *
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+let carData = undefined;
+let titanData = undefined;
+let langData = undefined;
+let lang = "fr";
 
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+
+//Start cartype as empty
+let carType = undefined;
+
+/**
+ * Load the car and titan data
+ */
+function preload() {
+  titanData = loadJSON("assets / data / greek_titans.json");
+  carData = loadJSON("assets / data / cars.json");
 }
 
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(600, 400);
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Display the current main text (either instructions or a car)
+ */
 function draw() {
+  background(0);
 
+  push();
+  fill("pink");
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text(carType + carName, width / 2, height / 2);
+  pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
+  //Choose Car Type
+  carType = random(carData.cars);
+  //Choose Car Name
+  carName = random(titanData.greek_titans);
 }
