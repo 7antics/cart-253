b431135af3056237d7bee491de52bf418bda68a2
Anna Tsirbas <annatsirbas@gmail.com>
2025-11-19 20:26:06 -0500

    Made a very impressive grid :)

--- Jams/Variation_Jam/Jam/js/script.js
+++ Jams/Variation_Jam/Jam/js/script.js
@@ -1,24 +1,46 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * UNDECIDED
+ * Anna Tsirbas
+ *
+ * Making three different variations of one game
  */
 
 "use strict";
 
+//Set up grid
+let CELL_SIZE = 50; //size of grid cell
+let NUM_COLS = 10; //number of columns
+let NUM_ROWS = 10; //number of rows
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ */
 function setup() {
-
+  createCanvas(550, 550);
 }
-
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ */
 function draw() {
+  background(0);
+  drawBoard();
+  drawCell();
+  console.log(drawCell);
+}
 
+function drawBoard() {
+  for (let a = 0; a < NUM_COLS; a++) {
+    for (let b = 0; b < NUM_ROWS; b++) {
+      drawCell(a, b);
+    }
+  }
+}
+
+function drawCell(a, b) {
+  push();
+  fill("White");
+  rectMode(CORNER);
+  rect(a * CELL_SIZE, b * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  pop();
 }
